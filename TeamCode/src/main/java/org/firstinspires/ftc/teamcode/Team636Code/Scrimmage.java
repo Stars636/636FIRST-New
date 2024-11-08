105789bcb00713c3a3c175a3fa35ed92712cf05a
Houston-Worlds <promise.egunjobi@wheelergmail.org>
2024-11-08 15:37:43 -0500

    New Code for testing pushbridge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team636Code/Scrimmage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team636Code/Scrimmage.java
@@ -8,19 +8,24 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Scrimmage extends LinearOpMode{
-    Servo Jamal;
-    Servo Ethan;
-
+    Servo rotatorJamal;
+    Servo clawEthan;
+    Servo pushRight;
+    Servo pushLeft;
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor rightBack = hardwareMap.get(DcMotor.class,"rightBack");
         DcMotor leftBack = hardwareMap.get(DcMotor.class,"leftBack");
         DcMotor rightFront = hardwareMap.get(DcMotor.class,"rightFront");
         DcMotor leftFront = hardwareMap.get(DcMotor.class,"leftFront");
-        Jamal = hardwareMap.get(Servo.class,"Servo1");
-        Ethan = hardwareMap.get(Servo.class,"Servo2");
-        double positionJamal = 0.0;
+        rotatorJamal = hardwareMap.get(Servo.class,"Servo1");
+        clawEthan = hardwareMap.get(Servo.class,"Servo2");
+        pushRight = hardwareMap.get(Servo.class,"pushRight");
+        pushLeft = hardwareMap.get(Servo.class,"pushLeft");
+        double positionJamal = 0.1;
         double positionEthan = 0.05;
+        double pushPositionRight = 0;
+        double pushPositionLeft = 1;
 
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -29,10 +34,9 @@ public class Scrimmage extends LinearOpMode{
 
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        boolean changeda = false;
-        boolean changedb = false;
-        boolean changedx = false;
-        boolean changedy = false;
+        boolean changedR = false;
+        boolean changedL = false;
+
 
         waitForStart();
 
@@ -40,33 +44,34 @@ public class Scrimmage extends LinearOpMode{
 
 
         while (opModeIsActive()) {
-            if (gamepad1.a && positionJamal <= 0.47) {
+            if (gamepad1.a && positionJamal <= 0.88) {
                 positionJamal += 0.01;
-                changeda = true;
-
 
             }
-            if (gamepad1.b && positionJamal >= 0.01) {
+            if (gamepad1.b && positionJamal >= 0.05) {
                 positionJamal -= 0.01;
-                changedb = true;
-            } //else if(!gamepad1.b) {
-               // changedb = false;
-            //}
+            }
 
             if (gamepad1.x && positionEthan <= 0.51500) {
                 positionEthan += 0.01;
-                changedx = true;
-
-
-            } //else if (!gamepad1.x) {
-              //  changedx = false;
-           // }
+            }
             if (gamepad1.y && positionEthan >= 0.01) {
                 positionEthan -= 0.01;
-                changedy = true;
-            } //else if(!gamepad1.y) {
-               // changedy = false;
-            //}
+            }
+            if (gamepad1.right_trigger != 0 && pushPositionRight < 1 && !changedR) {
+                pushPositionRight += 0.001;
+                pushPositionLeft -= 0.001;
+                changedR = true;
+            } else if(gamepad1.right_trigger == 0) {
+                changedR = false;
+            }
+            if (gamepad1.left_trigger != 0 && pushPositionLeft > 0 && !changedL) {
+                pushPositionRight -= 0.001;
+                pushPositionLeft += 0.001;
+                changedL = true;
+            } else if(gamepad1.left_trigger == 0) {
+                changedL = false;
+            }
 
             double joystickX = -gamepad1.left_stick_x;
             double joystickY = gamepad1.left_stick_y;
@@ -78,11 +83,15 @@ public class Scrimmage extends LinearOpMode{
             rightBack.setPower(joystickY + joystickX - joystickR);
             leftBack.setPower(joystickY - joystickX + joystickR);
 
-            Jamal.setPosition(positionJamal);
-            Ethan.setPosition(positionEthan);
+            rotatorJamal.setPosition(positionJamal);
+            clawEthan.setPosition(positionEthan);
+            //pushLeft.setPosition(pushPositionLeft); we will test one servo at a time
+            //pushRight.setPosition(pushPositionRight);
 
-            telemetry.addData("Jamal Position", Jamal.getPosition());
-            telemetry.addData("Ethan Position", Ethan.getPosition());
+            telemetry.addData("Jamal Position", rotatorJamal.getPosition());
+            telemetry.addData("Ethan Position", clawEthan.getPosition());
+            telemetry.addData("Ethan Position", pushLeft.getPosition());
+            telemetry.addData("Ethan Position", pushRight.getPosition());
             telemetry.update();
 
             idle();

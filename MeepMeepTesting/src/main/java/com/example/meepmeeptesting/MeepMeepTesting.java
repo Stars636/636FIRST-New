6c9ced5bb1f5b37ccf483c41dccc46f1a71c0334
Stars636 <promise.egunjobi@wheelergmail.org>
2024-12-21 18:18:48 -0500

    meep meep finally working?

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,5 +1,7 @@
 package com.example.meepmeeptesting;
 
+import static java.lang.Math.PI;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
@@ -9,26 +11,32 @@ import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
+        double xInitial = -44;
+        double yIntitial = -64;
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+
+
+        RoadRunnerBotEntity basketCalvin = new DefaultBotBuilder(meepMeep)
+                .setDimensions(12, 15)
+                .setStartPose(new Pose2d(xInitial, yIntitial, PI/2))
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(xInitial, yIntitial, PI/2))
+                        .splineToLinearHeading(new Pose2d(xInitial - 12, yIntitial + 12, Math.toRadians(45)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(xInitial - 4, yIntitial + 30, Math.toRadians(90)), Math.toRadians(45))
+                        .splineToLinearHeading(new Pose2d(xInitial - 12, yIntitial + 12, Math.toRadians(45)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(xInitial - 14, yIntitial + 30, Math.toRadians(90)), Math.toRadians(45))
+                        .splineToLinearHeading(new Pose2d(xInitial - 12, yIntitial + 12, Math.toRadians(45)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(xInitial - 20, yIntitial + 30, Math.toRadians(120)), Math.toRadians(45))
+                        .splineToLinearHeading(new Pose2d(xInitial - 12, yIntitial + 12, Math.toRadians(45)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(xInitial + 23, yIntitial + 64, Math.toRadians(540)), Math.toRadians(45))
                         .build());
-        
+
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(basketCalvin)
                 .start();
     }
 }

fc9db1ae8abeba95ceb6f7ebc3c3ef5e57431a6f
Houston-Worlds <promise.egunjobi@wheelergmail.org>
2025-01-09 22:21:47 -0500

    erm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackUps_TrashCan/CalvinPositionTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackUps_TrashCan/CalvinPositionTester.java
@@ -13,7 +13,7 @@ public class CalvinPositionTester extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         ElapsedTime et = new ElapsedTime();
 
-        Calvin functions = new Calvin(hardwareMap, telemetry);
+        Calvin calvin = new Calvin(hardwareMap, telemetry);
 
 
         //Initial!!
@@ -30,21 +30,21 @@ public class CalvinPositionTester extends LinearOpMode {
 
             //Activate  the intake
 
-            functions.activateIntake(gamepad2.a);
+            calvin.activateIntake(gamepad2.a);
             //Reverse. I chose this button(because the old bot had no equivalent), so if you want another one then do it
-            functions.activateEject(gamepad2.x);
+            calvin.activateEject(gamepad2.x);
             //Pushes the extendo. Positions need to be tested.
             //Test these positionsssss
-            functions.activateExtendo(gamepad2.right_trigger);
+            calvin.activateExtendo(gamepad2.right_trigger);
             //functions.switchScoring(gamepad1.x);
             //functions.passiveOrInitial(gamepad2.dpad_up);
             //code for moving claw
-            functions.activateClaw(gamepad2.y);
+            calvin.activateClaw(gamepad2.y);
             //rotates the claw. there is much better way to do this, but this works for now
             //you have to continuously hold to dunk it
-            functions.activateClawRotator(gamepad2.left_trigger);
+            calvin.activateClawRotator(gamepad2.left_trigger);
 
-            functions.activatePassiveOrInitial(gamepad2.dpad_up);
+            calvin.activatePassiveOrInitial(gamepad2.dpad_up);
 
             //functions.activateVerticalSlides(gamepad2.left_stick_y);
 
@@ -52,10 +52,10 @@ public class CalvinPositionTester extends LinearOpMode {
             double joystickY = gamepad1.left_stick_y;
             double joystickR = -gamepad1.right_stick_x;
 
-            functions.rightFrontCalvin.setPower(joystickY - joystickX - joystickR);
-            functions.leftFrontCalvin.setPower(joystickY + joystickX + joystickR);
-           functions.rightBackCalvin.setPower(joystickY + joystickX - joystickR);
-            functions.leftBackCalvin.setPower(joystickY - joystickX + joystickR);
+            calvin.rightFrontCalvin.setPower(joystickY - joystickX - joystickR);
+            calvin.leftFrontCalvin.setPower(joystickY + joystickX + joystickR);
+           calvin.rightBackCalvin.setPower(joystickY + joystickX - joystickR);
+            calvin.leftBackCalvin.setPower(joystickY - joystickX + joystickR);
 
 
            // telemetry.addData("MotorPosition",functions.verticalSlidesLeft.getCurrentPosition());
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalvinTeleOp/TeleOp_COPYFROMHERE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalvinTeleOp/TeleOp_COPYFROMHERE.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.CalvinTeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorImplEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.RobotAndHerHelpers.Calvin;
@@ -9,7 +12,7 @@ import org.firstinspires.ftc.teamcode.RobotAndHerHelpers.Calvin;
 @TeleOp
 public class TeleOp_COPYFROMHERE extends LinearOpMode {
 
-
+    DcMotorImplEx SlidesLeft, SlidesRight;
 
 
 
@@ -18,7 +21,18 @@ public class TeleOp_COPYFROMHERE extends LinearOpMode {
         ElapsedTime et = new ElapsedTime();
 
         Calvin calvin = new Calvin(hardwareMap, telemetry);
+        SlidesLeft = hardwareMap.get(DcMotorImplEx.class,"verticalSlidesLeft");
+        SlidesRight = hardwareMap.get(DcMotorImplEx.class,"verticalSlidesRight");
 
+        SlidesLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        SlidesLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        SlidesRight.setMode(DcMotorImplEx.RunMode.STOP_AND_RESET_ENCODER);
+        SlidesRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        SlidesLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        SlidesRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        SlidesLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 

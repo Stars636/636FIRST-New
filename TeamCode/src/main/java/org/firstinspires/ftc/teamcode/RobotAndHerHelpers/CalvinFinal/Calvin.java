b24a735fa3cf4b78080a2ec5b20867f8f8112c98
darsh7378274 <adarsh.presanna@gmail.com>
2025-01-24 17:27:38 -0500

    Update Calvin.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAndHerHelpers/CalvinFinal/Calvin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAndHerHelpers/CalvinFinal/Calvin.java
@@ -1,26 +1,18 @@
 package org.firstinspires.ftc.teamcode.RobotAndHerHelpers.CalvinFinal;
 
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorImplEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.PwmControl;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class Calvin {
     DcMotorEx leftFront, rightFront, leftBack, rightBack;
     DcMotorImplEx vSlidesLeft, vSlidesRight;
 
     DcMotorImplEx hangleft, hangRight;
 
-    ServoImplEx hClaw, hWrist, hElbow, hArm;
+    ServoImplEx intakeClaw, intakeWrist, intakeElbow, intakeArm;
 
-    ServoImplEx vClaw, vElbow, vWrist;
+    ServoImplEx depositClaw, depositElbow, depositWrist;
 
     ServoImplEx hSlidesLeft, hSlidesRight;
 
@@ -32,34 +24,34 @@ public class Calvin {
 
     public static int hangfinish; //add this to the current position of the hang motors
 
-    public static double vClawOpen = 0.17;
-    public static double vClawClosed = 0.30;
+    public static double depositClawOpen = 0.17;
+    public static double depositClawClosed = 0.30;
 
 
-    public static double vClawPassivePos = 0.65;
+    public static double depositClawPassivePos = 0.65;
 
-    public static double vClawPassiveRot = 0.43;
+    public static double depositClawPassiveRot = 0.43;
 
-    public static double vClawTransferPos = 0.9;
+    public static double depositClawTransferPos = 0.9;
 
-    public static double vClawTransferRot = 0.06;
+    public static double depositClawTransferRot = 0.06;
 
-    public static double vClawScorePos = 0.4;
+    public static double depositClawScorePos = 0.4;
 
-    public static double vClawScoreRot = 1;
+    public static double depositClawScoreRot = 1;
 
-    public static double vClawSpeciPos = 0;
+    public static double depositClawSpeciPos = 0;
 
-    public static double vClawSpeciRot = 1;
+    public static double depositClawSpeciRot = 1;
 
     public static double hSlidesInside = 0.99;
     public static double hSlidesOutside = 0.68;
-    public static double hClawOpen;
-    public static double hClawClosed;
-    public static double hClawTransferPos;
-    public static double hClawTransferRot;
-    public static double hClawPassivePos;
-    public static double hClawPassiveRot;
+    public static double intakeClawOpen;
+    public static double intakeClawClosed;
+    public static double intakeClawTransferPos;
+    public static double intakeClawTransferRot;
+    public static double intakeClawPassivePos;
+    public static double intakeClawPassiveRot;
 
     //public static double hClaw
 

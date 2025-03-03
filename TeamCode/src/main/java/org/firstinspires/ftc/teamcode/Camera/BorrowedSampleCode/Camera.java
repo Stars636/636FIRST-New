0fab8e24ab3985b90b102de48f8c6dd5551b7424
Houston-Worlds <promise.egunjobi@wheelergmail.org>
2025-03-03 11:58:16 -0500

    yay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/BorrowedSampleCode/Camera.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.Camera.BorrowedSampleCode;
-
-public class Camera {
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/BorrowedSampleCode/opencvSkystoneDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/BorrowedSampleCode/opencvSkystoneDetector.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Camera.BorrowedSampleCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -32,6 +33,7 @@ import java.util.List;
  *YES
  */
 @Autonomous(name= "opencvSkystoneDetector", group="Sky autonomous")
+@Disabled
 //courtesy of Bolts of Steel 3939
 public class opencvSkystoneDetector extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
